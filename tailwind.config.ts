355651502cf3301e9e33481ad0200524145ee450
hareame1947 <134369684+hakushi47@users.noreply.github.com>
2025-08-07 15:33:56 +0900

    ログインページ実装

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#E8DED6",
+        primary: "#A65D6D",
+        text: "#3F3A38",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      borderRadius: {
+        'lg': '0.625rem', // 10px
+        'md': '0.375rem', // 6px
+      }
+    },
+  },
+  plugins: [],
+}
